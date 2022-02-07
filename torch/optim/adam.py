81a0f066674c38cf4b3e9e82a51cba91c8ac49e7
Moiz Ahmad <76589636+moizca@users.noreply.github.com>
2022-02-07 01:24:32 -0800

    AMSGrad optimizer value in ADAM implementation
    As our research project uses "Amsgrad" optimizer instead of "Adam", we change the default value of "Amsgrad" in the constructor of "Adam" implementation to True, which is False in the original Pytorch repository.

--- torch/optim/adam.py
+++ torch/optim/adam.py
@@ -65,7 +65,7 @@ class Adam(Optimizer):
     """
 
     def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8,
-                 weight_decay=0, amsgrad=False, *, maximize: bool = False):
+                 weight_decay=0, amsgrad=True, *, maximize: bool = False):
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if not 0.0 <= eps:
